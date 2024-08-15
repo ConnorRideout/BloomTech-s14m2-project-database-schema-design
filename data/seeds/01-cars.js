641ca1f7709540eb3fd5cf1430ae7b8038e545a1
Connor Rideout <connorrideout13@gmail.com>
2024-08-14 20:44:25 -0400

    completed Sprint 14 Module 2 (stretch)

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,23 @@
-// STRETCH
+exports.seed = function (knex, Promise) {
+    return knex('cars').truncate()
+        .then(function () {
+            return knex('cars').insert([
+                {
+                    vin: '11111111111111111',
+                    make: 'Toyota',
+                    model: 'Camry',
+                    mileage: 12345,
+                    title: 'clean',
+                    transmission: 'automatic'
+                },
+                {
+                    vin: '22222222222222222',
+                    make: 'Subaru',
+                    model: 'Outback',
+                    mileage: 154987,
+                    title: 'salvage',
+                    transmission: 'manual'
+                }
+            ])
+        })
+}
